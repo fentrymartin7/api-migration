137c263b494724171c4eb02f0e9a0ce58d6f1ccb
fentrymartin7 <fentrymartin7@gmail.com>
2022-03-17 18:11:25 -0400

    Created a util package and java file to help establish connection to postgres database

---
+++ project0/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,28 @@
+package com.revature.util;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+
+	private static Connection con;
+	
+	public static Connection getConnectionFromFile() throws IOException, SQLException {
+		// to load url, username, password
+		Properties prop = new Properties();
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		prop.load(loader.getResourceAsStream("credentialss.properties"));
+			
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String password = prop.getProperty("password");
+			
+		if (con == null || con.isClosed()) {
+			con = DriverManager.getConnection(url, username, password);
+		}
+		return con;
+	}
+}
